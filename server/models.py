a111d534f5918b18585d53d9a9ec57836abb8124
cindy kivala <cindykivala@gmail.com>
2025-09-27 17:56:32 +0300

    Cart model debug restore

--- server/models.py
+++ server/models.py
@@ -85,7 +85,7 @@ class CartItem(db.Model):
     __tablename__ = "cart_items"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
     quantity = db.Column(db.Integer, default=1)
 
